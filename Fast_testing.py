25e651b53dd5971c7898b966942094ee03958d97
Peter Schade <peterfschade@gmail.com>
2024-11-22 23:45:02 -0500

    clear fast testing file

--- Fast_testing.py
+++ Fast_testing.py
@@ -1,40 +1,40 @@
+"""empty testing file for fast tensor backend"""
+#import minitorch
+#from minitorch import MathTestVariable, Tensor, TensorBackend, grad_check
+#import numpy as np
+#from numba import int32, re
 
-import minitorch
-from minitorch import MathTestVariable, Tensor, TensorBackend, grad_check
-import numpy as np
-from numba import int32, re
+#simpleTensorBackend = minitorch.TensorBackend(minitorch.SimpleOps)
+#fastTensorBackend = minitorch.TensorBackend(minitorch.FastOps)
 
-simpleTensorBackend = minitorch.TensorBackend(minitorch.SimpleOps)
-fastTensorBackend = minitorch.TensorBackend(minitorch.FastOps)
+#size_a = 2
+#size_b = 2
+#size_in = 2
+#x1 = [
+#            [[np.random.uniform(0, 2) for i in range(size_in)] for j in range(size_a)]
+#            for _ in range(4)
+#        ]
+#y1 = [
+#            [[np.random.uniform(0, 2) for i in range(size_b)] for j in range(size_in)]
+#            for _ in range(4)
+#        ]
 
-size_a = 2
-size_b = 2
-size_in = 2
-x1 = [
-            [[np.random.uniform(0, 2) for i in range(size_in)] for j in range(size_a)]
-            for _ in range(4)
-        ]
-y1 = [
-            [[np.random.uniform(0, 2) for i in range(size_b)] for j in range(size_in)]
-            for _ in range(4)
-        ]
+#print(np.matmul(x1, y1))
+#print(np.matmul(x1, y1).shape)
 
-print(np.matmul(x1, y1))
-print(np.matmul(x1, y1).shape)
-
-A = minitorch.tensor(x1, backend=fastTensorBackend)
-B = minitorch.tensor(y1, backend=fastTensorBackend)
-print('-------------------------')
-Z = A.__matmul__(B) 
-print(Z.shape)
-
-print(Z)
+#A = minitorch.tensor(x1, backend=fastTensorBackend)
+#B = minitorch.tensor(y1, backend=fastTensorBackend)
 #print('-------------------------')
-C = minitorch.tensor(x1, backend=simpleTensorBackend)
-D = minitorch.tensor(y1, backend=simpleTensorBackend)
+#Z = A.__matmul__(B) 
+#print(Z.shape)
+
+#print(Z)
+#print('-------------------------')
+#C = minitorch.tensor(x1, backend=simpleTensorBackend)
+#D = minitorch.tensor(y1, backend=simpleTensorBackend)
 #print("here")
-Z2 = C.__matmul__(D)
-print(Z2)
+#Z2 = C.__matmul__(D)
+#print(Z2)
 
 
 #A = Tensor(minitorch.TensorData([1,2,3,4], shape=(1,4)), backend=FastTensorBackend)
